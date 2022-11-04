0ba3aea9b64dc91f00b0cb6a4e8e938442468964
Andrey Lisovsky <mitolcore1234@gmail.com>
2022-11-04 19:07:04 +0300

    feature: added filter to the top-section

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,91 @@ data.forEach(({ city, country, imageUrl, name }) => {
 });
 homesSlider();
 reviewsSlider();
+
+//  filter
+
+const selectWrapper = document.querySelector('.select__wrapper');
+const filterWrapper = document.querySelector('.filter__wrapper');
+const filterItems = document.querySelectorAll('.filter__item');
+const adultsInput = document.getElementById('adults');
+const childrenInput = document.getElementById('children');
+const roomsInput = document.getElementById('room');
+const adultsDecBtn = document.getElementById('button-adults');
+const childrenDecBtn = document.getElementById('button-children');
+const roomsDecBtn = document.getElementById('button-rooms');
+const additionalContent = document.querySelector('.filter__additional-content');
+
+const showFilter = () => {
+  filterWrapper.classList.remove('hide');
+  filterWrapper.classList.add('show');
+};
+
+const hideFilter = () => {
+  filterWrapper.classList.remove('show');
+  filterWrapper.classList.add('hide');
+};
+
+selectWrapper.addEventListener('click', showFilter);
+
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.select__wrapper') && !e.target.closest('.filter__wrapper')) {
+    hideFilter();
+  }
+});
+
+adultsDecBtn.disabled = true;
+childrenDecBtn.disabled = true;
+roomsDecBtn.disabled = true;
+
+filterItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    const counter = e.target.closest('.filter__item').querySelector('.filter__item-counter');
+    const counterInput = e.target.closest('.filter__item').querySelector('.item__counter');
+    let value = counterInput.value;
+
+    function changeInputs(titleInputClass, rangeLow, rangeHigh, inputType, valueName, valueNames, differentCondition) {
+      if (
+        e.target.classList.contains('filter__button--inc') &&
+        counter.previousSibling.classList.contains(titleInputClass)
+      ) {
+        if (differentCondition) {
+          value++;
+          inputType.value = `${value} ${valueNames}`;
+        }
+      } else if (
+        e.target.classList.contains('filter__button--dec') &&
+        counter.previousSibling.classList.contains(titleInputClass)
+      ) {
+        if (value <= rangeLow) {
+          value;
+          inputType.value = `${value} ${valueName}`;
+        } else {
+          value--;
+          inputType.value = `${value} ${valueNames}`;
+
+          if (value === 1) {
+            inputType.value = `${value} ${valueName}`;
+          }
+        }
+      }
+    }
+
+    changeInputs(
+      'item__title--adults',
+      1,
+      30,
+      adultsInput,
+      'Adult',
+      'Adults',
+      'value >= rangeLow && value < rangeHigh',
+    );
+    changeInputs('item__title--rooms', 1, 30, roomsInput, 'Room', 'Rooms', 'value >= rangeLow && value < rangeHigh');
+    changeInputs('item__title--children', 0, 10, childrenInput, 'Children', 'Children', 'value < rangeLow');
+    counterInput.value = value;
+  });
+});
+
 // close adv
 
 const advert = document.querySelector('.advertisement');
